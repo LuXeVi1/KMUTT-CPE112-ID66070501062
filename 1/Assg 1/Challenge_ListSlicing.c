8c58739d5ecbe88b243eff3dd554b7b4cf1fa304
BuXeVi1Workspace <arkkhanirut.pandej@gmail.com>
2024-02-07 20:23:05 +0700

    update Challenge

--- 1/Assg 1/Challenge_ListSlicing.c
+++ 1/Assg 1/Challenge_ListSlicing.c
@@ -2,23 +2,41 @@
 // Arkkhanirut Pandej 66070501062
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int n;
     scanf("%d", &n);
-    int elements[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &elements[i]);
+
+    int* list = (int*)malloc(n * sizeof(int));
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &list[i]);
     }
+
     int start, end, step;
     scanf("%d %d %d", &start, &end, &step);
-    int printed = 0;
-    for (int i = start; i < end; i += step) {
-        printf("%d ", elements[i]);
-        printed = 1;
+
+        if (start < 0) start = n + start;
+    if (end < 0) end = n + end;
+
+    if (start < 0) start = 0;
+    if (end > n) end = n;
+
+    if (step > 0) {
+        for(int i = start; i < end; i += step) {
+            if(i >= 0 && i < n) {
+                printf("%d ", list[i]);
+            }
+        }
+    } else {
+        for(int i = start; i > end; i += step) {
+            if(i >= 0 && i < n) {
+                printf("%d ", list[i]);
+            }
+        }
     }
-    if (!printed) {
-        printf("empty");
-    }
+
+    free(list);
+
     return 0;
 }
